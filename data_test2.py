91c7105d9584a8e78be6af8b9f43b37501a389e6
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-01-28 21:26:43 +0700

    .

---
+++ data_test2.py
@@ -0,0 +1,53 @@
+import random
+import numpy as np
+import pandas as pd
+
+Choice = []
+Choice1 = []
+Result = []
+Week=[]
+
+for i in range(1000):
+    if i%2 == 1:
+        Result.append(random.uniform(13.00,14.00))
+    elif i%2 == 0:
+        Result.append(random.uniform(10.00,11.00))
+    Week.append(random.randint(0,5))
+    #print(i)
+    for j in range(9):
+        if i%2 == 1:
+            Choice1.append(random.randint(2,5))
+        elif i%2 == 0:
+            Choice1.append(random.randint(0,3))
+    Choice.append(Choice1)
+    Choice1 = []
+
+#print(Result)
+
+data = []
+dic={}
+for i,x in enumerate(Choice):
+    dic["Choice"] = Choice[i]
+    dic["Result"] = Result[i]
+    dic["Week"] = Week[i]
+    data.append(dic)
+    dic={}
+
+#print(data)
+
+########################################################################################################################################
+
+def Average(lst): 
+    return sum(lst) / len(lst) 
+
+output = []
+for i,x in enumerate(data):
+    #output.append([Average(data[i]["Choice"]) , data[i]["Result"]] )
+    output.append([Average(data[i]["Choice"]) , data[i]["Result"] * (data[i]["Week"]+1)] )
+    #output.append([Average(data[i]["Choice"]) * (data[i]["Week"]+1) , data[i]["Result"]] )
+
+
+#print(output)
+
+df = pd.DataFrame(output)
+df.to_csv (r'test_set_01.csv', index = False, header=True)

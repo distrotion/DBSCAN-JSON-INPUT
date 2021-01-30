133aedcf1d573ec6e7e6f3267c11007a710a20f3
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-01-30 23:58:36 +0700

    .

---
+++ API_test.py
@@ -0,0 +1,16 @@
+# hello.py
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, world! running on %s' % request.host
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    app.run(port=6000)
---
+++ API_test2.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET', 'POST'])
+def my_json():
+	if request.method == 'POST':
+		print(request.json)							
+		return jsonify(request.json)
+	return '200'
+
+if __name__ == '__main__':
+   app.run(debug = True,port=6000)

ee3dae7c71d9b5b4727101a6b2692727eb562025
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-02-02 18:59:49 +0700

    .

--- API_test4.py
+++ API_test4.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, jsonify
 import json
 import requests
+import time
 
 
 app = Flask(__name__)
 
 @app.route('/test', methods=['POST'])
 def my_json():
-	
-	res = requests.post('https://us-central1-nicetynine-mind-fit.cloudfunctions.net/setMindTestWeekyScore', json={})
+	for i in range(100):
+		res = requests.post('https://us-central1-nicetynine-mind-fit.cloudfunctions.net/setMindTestWeekyScore', json={})
 
-	print(res.json())	
+		print("ROUND: {} -->".format(i),res.json())	
 
-	res2 = requests.post('https://dbscan-deploy-nmdlf3uxjq-as.a.run.app/DBSCAN', json=res.json())	
+		res2 = requests.post('https://dbscan-deploy-nmdlf3uxjq-as.a.run.app/DBSCAN', json=res.json())	
 
-	print(res2.json())					
+		#print(res2.json())			
+
+		time.sleep(2)		
 	return jsonify(res2.json())
 	
 
